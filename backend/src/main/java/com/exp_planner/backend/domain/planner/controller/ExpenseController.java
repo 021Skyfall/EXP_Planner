fa0f9357a16d39c35231ecd47fc064385180f763
Jeremiah Lee <021skyfall@google.com>
2025-06-25 15:16:26 +0900

    docs(planner): Add Swagger and document POST endpoint

--- backend/src/main/java/com/exp_planner/backend/domain/planner/controller/ExpenseController.java
+++ backend/src/main/java/com/exp_planner/backend/domain/planner/controller/ExpenseController.java
@@ -4,6 +4,8 @@ import com.exp_planner.backend.domain.planner.dto.ExpenseDto;
 import com.exp_planner.backend.domain.planner.entity.Expense;
 import com.exp_planner.backend.domain.planner.mapper.ExpenseMapper;
 import com.exp_planner.backend.domain.planner.service.ExpenseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +17,15 @@ import java.net.URI;
 @RestController
 @RequestMapping("/exp/expenses")
 @RequiredArgsConstructor
+// docs
+@Tag(name = "Expense", description = "지출 api")
+// logs
 @Slf4j
 public class ExpenseController {
     private final ExpenseService service;
     private final ExpenseMapper mapper;
 
+    @Operation(summary = "Expense post api", description = "지출 내역 등록")
     @PostMapping("/post")
     public ResponseEntity<ExpenseDto.Response> postExpense(@RequestBody ExpenseDto.Post postDto) {
         Expense savedExpense = service.createExpense(postDto);
