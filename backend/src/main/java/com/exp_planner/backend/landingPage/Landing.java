273d659832dc3e1d99c011df86a02efc7565da64
Jeremiah Lee <021skyfall@google.com>
2025-10-23 11:14:08 +0900

    create landing page

--- backend/src/main/java/com/exp_planner/backend/landingPage/Landing.java
+++ backend/src/main/java/com/exp_planner/backend/landingPage/Landing.java
@@ -1,4 +1,4 @@
-package com.exp_planner.backend;
+package com.exp_planner.backend.landingPage;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
