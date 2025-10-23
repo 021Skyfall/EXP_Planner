27ad8c18da91c076f0269b4a029d4669291e25b1
Jeremiah Lee <021skyfall@google.com>
2025-10-23 11:06:40 +0900

    create landing page

---
+++ backend/src/main/java/com/exp_planner/backend/landingPage/Landing.java
@@ -0,0 +1,15 @@
+package com.exp_planner.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Landing {
+
+    @GetMapping("/landing")
+    public String landing() {
+        return "Hello from Spring Boot Backend! \nThis is the landing page of the backend.";
+    }
+}
